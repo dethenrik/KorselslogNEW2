bcfe990143258c1979edb2136f9990ed206a0baa
dethenrik <98741878+dethenrik@users.noreply.github.com>
2023-01-12 22:40:33 +0100

    Add files via upload

---
+++ winformKørselslog/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winformKørselslog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult iExit;
+            try
+            {
+
+                iExit = MessageBox.Show("Confirm if you want to exit", "KørselsLog",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (iExit == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach(Control c in panel6.Controls)
+                {
+                    if (c is TextBox)
+                        ((TextBox)c).Clear();
+                }
+                txtSearch.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
